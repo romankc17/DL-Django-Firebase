2d947ef23fbd6c418f5a3ec3dca1b6653afbe951
romankc17 <romanchhetri02@gmail.com>
2021-10-24 22:12:40 +0545

    gitignore updated

---
+++ firebase_db/firebaseConfig.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+    'apiKey': "AIzaSyC0xnr3u6UdBSr8B92RrmuhSHHWOxoyEWU",
+    'authDomain': "drivingliscence-72223.firebaseapp.com",
+    'projectId': "drivingliscence-72223",
+    'storageBucket': "drivingliscence-72223.appspot.com",
+    'messagingSenderId': "590919060724",
+    'appId': "1:590919060724:web:f467b1e6f946d44a15a2a1",
+    'measurementId': "G-TZWEBNE6SN",
+    'databaseURL':'https://drivingliscence-72223-default-rtdb.firebaseio.com'
+}
